54f631a2550254077d92cc43f034617576ab5eac
erianne <erianne.farias@gmail.com>
2021-01-07 14:51:54 -0300

    add chapter6

---
+++ py4e_coursera/chapter_6.py
@@ -0,0 +1,14 @@
+
+"""
+    Exercise 1: Write a while loop that starts
+    at the last character in the string and works
+    its way backwards to the first character in the string,
+    sprinting each letter on a separate line, except backwards
+"""
+
+fruit = "banana"
+index = -1
+while index > -(len(fruit)+1):
+    letter = fruit[index]
+    print(letter)
+    index = index - 1
